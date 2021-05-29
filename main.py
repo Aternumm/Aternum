26e30d301774dd4c25093abc76271236b87fc372
Aternum <gamedomination228@gmail.com>
2021-05-29 18:33:47 +0300

    First Version

---
+++ main.py
@@ -0,0 +1,59 @@
+import telebot
+import config
+
+from flask import Flask, request
+import os
+
+bot = telebot.TeleBot(config.TOKEN)
+
+app = Flask(__name__)
+
+courses = {
+    'Python': 'python.org',
+    'JS': 'learn.javascript.ru',
+}
+python = {
+    'print': 'python.org',
+    'input': 'python.org',
+}
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Ты выбрал команду старт' + str(message.chat))
+
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    bot.send_message(message.chat.id, 'Доступные команды: \n /help \n /start \n /courses')
+
+@bot.message_handler(commands=['courses'])
+def courses_message(message):
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+    for item in courses:
+        url_button = telebot.types.InlineKeyboardButton(text=item, url=courses[item])
+        keyboard.add(url_button)
+    bot.send_message(message.chat.id, 'Доступные курсы:', reply_markup=keyboard)
+
+@bot.message_handler(commands=['python'])
+def courses_message(message):
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+    for item in python:
+        url_button = telebot.types.InlineKeyboardButton(text=item, url=python[item])
+        keyboard.add(url_button)
+    bot.send_message(message.chat.id, 'Доступные курсы:', reply_markup=keyboard)
+
+
+@app.route('/' + config.TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "Python Telegram Bot 29-05-2021", 200
+
+
+@app.route('/')
+def main():
+    bot.remove_webhook()
+    bot.set_webhook(url='*' + config.TOKEN)
+    return "Python Telegram Bot", 200
+
+
+if __name__ == 'main':
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
